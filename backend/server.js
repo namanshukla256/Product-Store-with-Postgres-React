3db798a327124c5202a6cd8272d8ed854e4ea45d
Naman Shukla <namanshukla256@gmail.com>
2025-03-04 22:02:12 +0530

    gg

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import productRoutes from "./routes/productRoutes.js";
+
 dotenv.config();
 
 const app = express();
@@ -14,10 +16,7 @@ app.use(cors());
 app.use(helmet()); // helmet is a security middleware that helps you protect your app by setting various HTTP headers.
 app.use(morgan("dev")); // log the request
 
-app.get("/test", (req, res) => {
-    console.log(res.getHeaders());
-    res.send("Hello from the backend");
-}) 
+app.use("/api/products", productRoutes); 
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
