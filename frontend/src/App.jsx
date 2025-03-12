39a1f7663b13fe0984bb80d2c00b2e0e9bdc94ac
Naman Shukla <namanshukla256@gmail.com>
2025-03-12 23:30:55 +0530

    gg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
+
+import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 
-function App () {
-  return (
-    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+import { Routes, Route } from "react-router-dom";
+import { useThemeStore } from "./store/useThemeStore";
 
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  const { theme } = useThemeStore();
+
+  return (
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
       <Navbar />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/product/:id" element={<ProductPage />}/>
+        <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
+
+      <Toaster />
     </div>
   );
 }
